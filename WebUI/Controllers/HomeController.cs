212264548f5ada089c9120f278d47e394b938892
aleksandr-st-novikov <aleksandr.st.novikov@gmail.com>
2016-09-21 21:55:24 +0300

    В AccountController отключил ненужные методы.

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -20,20 +20,6 @@ namespace WebUI.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         [AllowAnonymous]
         public ActionResult Register()
         {
